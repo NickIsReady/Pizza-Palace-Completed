31591d4fa41614ee23b4f9f6578c20548ae5d075
Thomas Shortt <tmshortt07@gmail.com>
2017-05-09 04:36:33 +0000

    Modified MeatLoversPizza

--- src/asgn2Pizzas/MeatLoversPizza.java
+++ src/asgn2Pizzas/MeatLoversPizza.java
@@ -10,11 +10,14 @@ import asgn2Exceptions.PizzaException;
  *  The meat lovers pizza has certain toppings listed in Section 5.1 of the Assignment Specification Document.  
  *  A description of the class's fields and their constraints is provided in Section 5.1 of the Assignment Specification.
  * 
- * @author PersonA
+ * @author Thomas Shortt (n8854742)
  *
  */
 public class MeatLoversPizza extends Pizza {
 
+	private LocalTime openingTime = LocalTime.of(19, 00);
+	private LocalTime closingTime = LocalTime.of(23, 00);
+	
 	/**
 	 * 
 	 *  This class represents a meat lovers pizza made at the  Pizza Palace restaurant. The meat lovers pizza has certain
@@ -32,7 +35,14 @@ public class MeatLoversPizza extends Pizza {
 	 *
 	 */
 	public MeatLoversPizza(int quantity, LocalTime orderTime, LocalTime deliveryTime) throws PizzaException {
-		// TO DO
+		super(quantity, deliveryTime, deliveryTime, "Meat Lovers", 12.00);
+		if (quantity < 1 || quantity > 10) {
+			throw new PizzaException("Invalid quantity!");
+		} else if (orderTime.isBefore(openingTime) == true || orderTime.isAfter(closingTime) == true) {
+			throw new PizzaException("Order time is invalid!");
+		} else if (deliveryTime.isBefore(orderTime.plusMinutes(10)) == true || deliveryTime.isAfter(orderTime.plusHours(1)) == true) {
+			throw new PizzaException("Delivery time is invalid!");
+		}
 	}
 
 }
