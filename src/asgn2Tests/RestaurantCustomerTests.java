0f193975bbc6a490fd31ef85c34c1c3baee6428b
Thomas Shortt <tmshortt07@gmail.com>
2017-05-09 04:42:13 +0000

    RestaurantCustomerTests

--- src/asgn2Tests/RestaurantCustomerTests.java
+++ src/asgn2Tests/RestaurantCustomerTests.java
@@ -1,12 +1,45 @@
 package asgn2Tests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * A class that that tests the methods relating to the handling of Customer objects in the asgn2Restaurant.PizzaRestaurant
  * class as well as processLog and resetDetails.
  * 
- * @author Person A
+ * @author Thomas Shortt (n8854742)
  */
 public class RestaurantCustomerTests {
-	// TO DO
+	
+	@Before
+	public void setupRestaurantCustomer() {
+		
+	}
+	
+	@Test
+	public void testProcessLog() {
+		
+	}
+	
+	@Test
+	public void testGetCustomerByIndex() {
+		
+	}
+	
+	@Test
+	public void testGetNumCustomerOrders() {
+		
+	}
+	
+	@Test
+	public void testGetTotalDeliveryDistance() {
+		
+	}
+	
+	@Test
+	public void testResetDetails() {
+		
+	}
 }
