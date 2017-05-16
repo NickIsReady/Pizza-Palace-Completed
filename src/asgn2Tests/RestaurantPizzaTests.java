1178ae0d5b8032d022a7a7a0ede6e39d21fe6fde
QUTAD\n9171550 <nickconstantine123@gmail.com>
2017-05-16 11:45:05 +1000

    pizza restaurant test so far

--- src/asgn2Tests/RestaurantPizzaTests.java
+++ src/asgn2Tests/RestaurantPizzaTests.java
@@ -1,6 +1,17 @@
 package asgn2Tests;
 
 
+import java.time.LocalTime;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import asgn2Exceptions.CustomerException;
+import asgn2Exceptions.LogHandlerException;
+import asgn2Exceptions.PizzaException;
+import asgn2Pizzas.Pizza;
+import asgn2Restaurant.PizzaRestaurant;
 /**
  * A class that tests the methods relating to the handling of Pizza objects in the asgn2Restaurant.PizzaRestaurant class as well as
  * processLog and resetDetails.
@@ -9,5 +20,21 @@ package asgn2Tests;
  *
  */
 public class RestaurantPizzaTests {
-	// TO DO
+	
+	private PizzaRestaurant RestaurantA;
+	
+	@Before
+	public void InitialisePizzaRestuarant() throws PizzaException{
+		RestaurantA = new PizzaRestaurant();
+	}
+	
+
+	@Test
+	public void TestProcessLogOne() throws CustomerException, PizzaException, LogHandlerException{
+		assertTrue(RestaurantA.processLog("logs/20170101.txt"));
+	}
+	
+	
+	
+	
 }
