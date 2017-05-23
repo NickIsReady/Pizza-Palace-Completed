cb2a2f8035ce01bfb57d7d8e832beefb626cf809
QUTAD\n9171550 <nickconstantine123@gmail.com>
2017-05-23 10:40:53 +1000

    updated tests pizza restaurant

--- src/asgn2Tests/RestaurantPizzaTests.java
+++ src/asgn2Tests/RestaurantPizzaTests.java
@@ -11,10 +11,9 @@ import asgn2Exceptions.CustomerException;
 import asgn2Exceptions.LogHandlerException;
 import asgn2Exceptions.PizzaException;
 import asgn2Pizzas.Pizza;
-import asgn2Customers.Customer;
-import asgn2Customers.DriverDeliveryCustomer;
-import asgn2Customers.DroneDeliveryCustomer;
-import asgn2Customers.PickUpCustomer;
+import asgn2Pizzas.MargheritaPizza;
+import asgn2Pizzas.MeatLoversPizza;
+import asgn2Pizzas.VegetarianPizza;
 import asgn2Restaurant.PizzaRestaurant;
 /**
  * A class that tests the methods relating to the handling of Pizza objects in the asgn2Restaurant.PizzaRestaurant class as well as
@@ -32,7 +31,7 @@ public class RestaurantPizzaTests {
 		RestaurantA = new PizzaRestaurant();
 	}
 	
-
+	//Process log files
 	@Test
 	public void TestProcessLogOne() throws CustomerException, PizzaException, LogHandlerException{
 		assertTrue(RestaurantA.processLog("logs/20170101.txt"));
@@ -46,43 +45,68 @@ public class RestaurantPizzaTests {
 		assertTrue(RestaurantA.processLog("logs/20170103.txt"));
 	}
 	
-	/*//Invalid Log files
-	@Test(expected = CustomerException.class)
+	//Invalid Log files
+	@Test (expected = PizzaException.class)
 	public void TestProcessLogInvalid() throws CustomerException, PizzaException, LogHandlerException{
 		assertTrue(RestaurantA.processLog("logs/InvalidLog.txt"));
 	}
-	*/
+	
 	@Test 
-	public void GetCustomerByIndexLogFileOne() throws CustomerException, PizzaException, LogHandlerException{
+	public void GetPizzaByIndexLogFileOne() throws CustomerException, PizzaException, LogHandlerException{
 		RestaurantA.processLog("logs/20170101.txt");
 		
-		Customer FirstCustomer = new DriverDeliveryCustomer("Casey Jones", "0123456789", 5, 5);
-		assertEquals(RestaurantA.getCustomerByIndex(0), FirstCustomer);
+		Pizza FirstPizza = new VegetarianPizza(2, LocalTime.of(19, 00), LocalTime.of(19, 20));
+		assertEquals(RestaurantA.getPizzaByIndex(0), FirstPizza);
 		
-		Customer SecondCustomer = new DroneDeliveryCustomer("April O'Neal", "0987654321", 3, 4);
-		assertEquals(RestaurantA.getCustomerByIndex(1), SecondCustomer);
+		Pizza SecondPizza = new MargheritaPizza(1, LocalTime.of(20, 00), LocalTime.of(20, 25));
+		assertEquals(RestaurantA.getPizzaByIndex(1), SecondPizza);
 		
-		Customer ThirdCustomer = new PickUpCustomer("Oroku Saki", "0111222333", 0, 0);
-		assertEquals(RestaurantA.getCustomerByIndex(2), ThirdCustomer);
+		Pizza ThirdPizza = new MeatLoversPizza(3, LocalTime.of(21, 00), LocalTime.of(21, 35));
+		assertEquals(RestaurantA.getPizzaByIndex(2), ThirdPizza);
+	}
+	
+	//Invalid index tests
+	@Test(expected = PizzaException.class)
+	public void IndexOutOfRangeNegative() throws CustomerException, PizzaException, LogHandlerException{
+		RestaurantA.processLog("logs/20170101.txt");
+		Pizza InvalidPizza = RestaurantA.getPizzaByIndex(-1);
+	}
+	
+	@Test(expected = PizzaException.class)
+	public void IndexOutOfRangeAboveMax() throws CustomerException, PizzaException, LogHandlerException{
+		RestaurantA.processLog("logs/20170101.txt");
+		Pizza InvalidPizza = RestaurantA.getPizzaByIndex(5);
 	}
 	
 	//Get Number of Pizza Orders
 	@Test 
 	public void GetNumOfPizzaOrdersLogFileOne() throws CustomerException, PizzaException, LogHandlerException{
 		RestaurantA.processLog("logs/20170101.txt");
-		
+		assertEquals(RestaurantA.getNumPizzaOrders(), 3);
 	}
 	
 	@Test 
 	public void GetNumOfPizzaOrdersLogFileTwo() throws CustomerException, PizzaException, LogHandlerException{
 		RestaurantA.processLog("logs/20170102.txt");
-		
+		assertEquals(RestaurantA.getNumPizzaOrders(), 10);
 	}
 	
 	@Test 
 	public void GetNumOfPizzaOrdersLogFileThree() throws CustomerException, PizzaException, LogHandlerException{
 		RestaurantA.processLog("logs/20170103.txt");
-		
+		assertEquals(RestaurantA.getNumPizzaOrders(), 100);
 	}
+	
+	//GetTotalProfit
+	@Test 
+	public void GetTotalProfitLogFileOne() throws CustomerException, PizzaException, LogHandlerException{
+		RestaurantA.processLog("logs/20170101.txt");
+		double FirstCustomerProfit = RestaurantA.getPizzaByIndex(0).getOrderProfit();
+		double SecondCustomerProfit = RestaurantA.getPizzaByIndex(1).getOrderProfit();
+		double ThirdCustomerProfit = RestaurantA.getPizzaByIndex(2).getOrderProfit();
+		double TotalProfit = FirstCustomerProfit + SecondCustomerProfit + ThirdCustomerProfit;
+		assertEquals(RestaurantA.getTotalProfit(),TotalProfit,0);
+	}
+	
 	
 }
